036c190e08e4b2d27a9ee07724d829d6ed6bd12e
utherbit <utherbit@gmail.com>
2025-01-31 21:13:10 +0300

    feat: added go generate in template

--- template.go
+++ template.go
@@ -12,6 +12,7 @@ import (
 {{- end }}
 )
 
+//go:generate go run klad.rupu.ru/rupuru/eda/backend/cmd/gen/transfer {{.StructName}}
 type {{.StructName}}Transfer struct{ {{.StructName}} }
 
 func (t *{{.StructName}}Transfer) Base() {{.StructName}} { return t.{{.StructName}} }
